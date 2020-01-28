7cbddf22c90d4a1b3fb2bb51c275e65277684b8a
Adan Alvarado <adan.alvarado7@gmail.com>
2020-01-27 16:43:08 -0800

    Create 2d-array-hourglass-sum.js

---
+++ warm-up/2d-array-hourglass-sum.js
@@ -0,0 +1,44 @@
+let hourglassSum = (arr) => {
+  let sum;
+
+  let relativePositions = [
+    [-1, -1],
+    [-1,  0],
+    [-1,  1],
+    [0,   0],
+    [1,  -1],
+    [1,   0],
+    [1,   1]
+  ];
+
+  let validIndexes = (x, y) => {
+    if (x < 1 || x >= arr.length - 1)
+      return false;
+    if (y < 1 || y >= arr.length - 1)
+      return false;
+
+    return true;
+  };
+
+  arr.forEach((xArr, x) => {
+    xArr.forEach((_, y) => {
+      let hourGlassSum;
+
+      if(validIndexes(x, y)) {
+        relativePositions.forEach((point) => {
+          if(typeof(hourGlassSum) === 'undefined') {
+            hourGlassSum = arr[x + point[0]][y + point[1]];
+          } else {
+            hourGlassSum = hourGlassSum + arr[x + point[0]][y + point[1]];
+          }
+        })
+      }
+
+      if (typeof(sum) === 'undefined' || hourGlassSum > sum) {
+        sum = hourGlassSum;
+      }
+    });
+  });
+
+  return sum;
+}
